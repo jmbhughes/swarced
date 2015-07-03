6d9d36aff7db85e73651056274d4732953dbe74f
Marcus Hughes <jmh3@williams.edu>
2015-07-03 12:53:30 -0400

    updated plotting utilities

--- swarced.py
+++ swarced.py
@@ -1,17 +1,13 @@
 from __future__ import division, print_function
 from matplotlib import rcParams
 rcParams["savefig.dpi"] = 100
-rcParams["font.size"] = 20
-import h5py
-import transit
+rcParams["font.size"] = 10
 import numpy as np
 import matplotlib.pyplot as pl
 from matplotlib.ticker import MaxNLocator
-import ketu
-import urllib2
-import sys
+import ketu, urllib2, sys, pickle, transit, h5py
 from astropy.io import fits
-import pickle
+import remove_EB as remEB
 
 def build_query(epicID, campaign, time_spacing=0.02, durations=[0.05,0.1,0.2]\
  ,min_period = 0.5,max_period=70.0,npeaks=3,path="DEFAULT",fn="DEFAULT"):
@@ -28,6 +24,7 @@ def build_query(epicID, campaign, time_spacing=0.02, durations=[0.05,0.1,0.2]\
             fn--the filename of the lightcurve, if default us /k2_data
     '''
     epicID, campaign = str(epicID), str(campaign)
+    #This is just a lazy method to format the default directory for my tests
     if path == "DEFAULT":
         path = "/k2_data/lightcurves/" + "c" + campaign + "/"
         path = path + epicID[0:4] + "00000/" + epicID[4:6] + "000/"
@@ -37,6 +34,7 @@ def build_query(epicID, campaign, time_spacing=0.02, durations=[0.05,0.1,0.2]\
         fn = "ktwo" + epicID + "-c0" + campaign + "_lpd-lc.fits"
     else:
         fn = fn
+    #Construct the dictionary query
 	q = dict(
         light_curve_file=path+fn,
         basis_file = "/k2_data/elcs/c" + campaign + ".h5",
@@ -57,8 +55,10 @@ def get_query(epicID, campaign):
         campaign---the K2 campaign of that EPIC designation
     '''
     epicID, campaign = str(epicID), str(campaign)
+    #Default path to lightcurve
     path = "/k2_data/lightcurves/" + "c" + campaign + "/" 
-    path += epicID[0:4] + "00000/" + epicID[4:6] + "000/"  
+    path += epicID[0:4] + "00000/" + epicID[4:6] + "000/" 
+    #Construct the dictionary query object
     q = dict(
         light_curve_file = path + "ktwo" + epicID + "-c0" + campaign + "_lpd-lc.fits",
         #initial_time=1975.,
@@ -96,7 +96,7 @@ def analyze(query,cache=False):
         pipe = ketu.OneDSearch(pipe,cache=False)
         pipe = ketu.TwoDSearch(pipe,cache=False)
         pipe = ketu.PeakDetect(pipe,cache=False)
-    else:
+    else:#do cache!
         pipe = ketu.k2.Data(basepath=cache)
         pipe = ketu.k2.Likelihood(pipe)
         pipe = ketu.OneDSearch(pipe)
@@ -110,13 +110,19 @@ def retrieve(epicID, campaign, inpath="/k2_data/lightcurves/"):
     '''Provides access to the time and flux data from a light curve given the EPIC ID and campaign'''
     campaign = str(campaign)
     epicID = str(epicID)
+    #if it's in the default path
     if inpath == "/k2_data/lightcurves/":
         path = inpath + "c" + campaign + "/" +  epicID[0:4] + "00000/" + epicID[4:6] + "000/" 
-    else:
+    else:#designate the full path
         path = inpath
+    #construct the filename
     fn = path + "ktwo" + epicID + "-c0" + campaign + "_lpd-lc.fits"
     f = fits.open(fn)
-    return f[1].data['time'] + f[1].header['BJDREFI'],f[1].data['flux']
+    aperture = np.argmin(f[2].data['cdpp6'])
+    time, flux = f[1].data['time'] + f[1].header['BJDREFI'], f[1].data['flux'][:,aperture]
+    quality = f[1].data['quality']
+    m = np.isfinite(time) * np.isfinite(flux) * (quality==0)
+    return time[m], flux[m]
 
 def edit(mask, epicID, campaign, inpath="/k2_data/lightcurves/", outpath="/k2_data/eb_removed/"):
     '''Allows direct alteration of a lightcurve by applying a mask to a K2 lightcurve'''
@@ -127,6 +133,7 @@ def edit(mask, epicID, campaign, inpath="/k2_data/lightcurves/", outpath="/k2_da
         path = inpath
     fn = "ktwo" + epicID + "-c0" + campaign + "_lpd-lc.fits"
     f = fits.open(path + fn)
+    #apply the mask to all the columns
     f[1].data = f[1].data[mask]
     f.writeto(outpath + fn, clobber=True)
     return
@@ -149,4 +156,25 @@ def plot_periodogram(result):
         ax.plot(x0, y0, ".r")
         ax.annotate("{0:.2f}".format(x0), xy=(x0, y0), ha="center",fontsize=10,
                     xytext=(10, 5), textcoords="offset points")
+    pl.show()
+    
+def plot_phase(epicID,campaign,period, t0):
+    epicID,campaign = str(epicID),str(campaign)
+    time, flux = retrieve(epicID,campaign)
+    fig = pl.figure(figsize=(5 * 1.61803398875,5))
+    pl.title("EPIC " + epicID)
+    phase = remEB.find_phase(time, period, t0)
+    pl.plot((phase + 0.25) % 1 - 0.25,flux,'k.',ms=10)
+    pl.xlabel("Phase")
+    pl.ylabel("FM15 Flux")
+    pl.show()
+    
+def plot_lc(epicID, campaign):
+    epicID,campaign = str(epicID),str(campaign)
+    time, flux = retrieve(epicID,campaign)
+    fig = pl.figure(figsize=(5 * 1.61803398875,5))
+    pl.title("EPIC " + epicID)
+    pl.plot(time,flux,'k.',ms=10)
+    pl.xlabel("Time (BJD)")
+    pl.ylabel("FM15 Flux")
     pl.show()
